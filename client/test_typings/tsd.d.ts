9a24aae91a1cc295b1e0e87c2e689365d8bf8f0f
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-05-14 16:34:57 -0400

    Reset

--- client/test_typings/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="jasmine/jasmine.d.ts" />
-/// <reference path="angular-protractor/angular-protractor.d.ts" />
-/// <reference path="selenium-webdriver/selenium-webdriver.d.ts" />
